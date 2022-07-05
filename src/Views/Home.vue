7986cb5233355babce58567d7b235c3ae31befd9
Erick Rosa <erickpmotta@gmail.com>
2022-07-04 23:36:28 -0300

    Adding image to each pokemon

--- src/Views/Home.vue
+++ src/Views/Home.vue
@@ -12,26 +12,30 @@ const getPokemons = async () => {
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
   const data = await response.json();
   pokemons.value = data.results;
-  console.log(pokemons.value);
+
   laoding.value = false;
-};
+  //add image to each pokemon
 
-const selectPokemon = (pokemon: any) => {
-  loadingSelectedPokemons.value = true;
-  const response = fetch(pokemon.url).then((response) => response.json());
-  response.then((data) => {
-    loadingSelectedPokemons.value = false;
-    selectedPokemon.value = data;
+  pokemons.value.forEach((pokemon: { url: any; image: any }) => {
+    fetch(pokemon.url)
+      .then((response) => response.json())
+      .then((data) => {
+        pokemon.image = data.sprites.front_default;
+      });
   });
-};
 
+  console.log(pokemons.value);
+};
 
 const filterPokemonsComputed = computed(() => {
-  return pokemons.value.filter((pokemon: any) => {
-    return pokemon.name.toLowerCase().includes(search.value.toLowerCase());
-  });
-});
+  if (pokemons.value && search.value) {
+    return pokemons.value.filter((pokemon: any) => {
+      return pokemon.name.toLowerCase().includes(search.value.toLowerCase());
+    });
+  }
 
+  return pokemons.value;
+});
 
 onMounted(() => {
   getPokemons();
@@ -40,48 +44,27 @@ onMounted(() => {
 
 <template>
   <div class="bg-slate-500">
-  
-<input
-type="text"
-v-model="search"
-
->
+    <input
+      type="text"
+      v-model="search"
+      class="bg-transparent border-none w-full placeholder-gray-500 text-gray-900 rounded-lg py-3 px-4 mb-3 leading-tight focus:outline-none"
+    />
 
     <ul class="grid grid-cols-1 gap-5">
       <li
         v-for="pokemon in filterPokemonsComputed"
         :key="pokemon.name"
-        @click="selectPokemon(pokemon)"
         class="bg-slate-100 rounded-lg p-2"
       >
+        <img :src="pokemon.image" class="h-24 w-24 rounded-full" />
         <p class="text-sm text-slate-700">
           {{ pokemon.name }}
         </p>
       </li>
     </ul>
-
   </div>
 
-<!-- render search results !--->
-
-<div class="container pokemons">
-
-    <div class="pokemon" :key="pokemon.name" v-for="pokemon in filterPokemonsComputed">
-        <div class="pokemon-name">
-        {{ pokemon.name }}
-        </div>
-        <div class="pokemon-image">
-        <img :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.name}.png`" />
-        </div>
-        <div class="pokemon-info">
-        <button @click="selectPokemon(pokemon)">More info</button>
-        </div>
-    </div>
-
-
-</div>
-
-
+  <!-- render search results !--->
 </template>
 
 <style></style>
